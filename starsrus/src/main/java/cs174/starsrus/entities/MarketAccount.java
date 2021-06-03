602c9ad4b4ebc9d8dec1fd9e35d46acfcc8d0ff0
Edwin Varela <edwinvarela@AD.isc.ucsb.edu>
2021-06-02 23:32:29 -0700

    EV - Remaining Entities

--- starsrus/src/main/java/cs174/starsrus/entities/MarketAccount.java
+++ starsrus/src/main/java/cs174/starsrus/entities/MarketAccount.java
@@ -1,17 +1,56 @@
 package cs174.starsrus.entities;
 
-import java.math.BigDecimal;
-import java.util.List;
 
-public class MarketAccount extends Account {
-    List<MarketAccountTransaction> transactionList;
-    public void deposit(int numberOfStocks) {
-        //TODO
-        
+public class MarketAccount {
+
+    private int market_account_id;
+    private float balance;
+    private String balance_date;
+    private String account_date;
+    private String username;
+
+
+     public Withdraw(){}
+
+
+    public int get_market_account_id() {
+        return market_account_id;
     }
 
-    public void withdraw(int numberOfStocks) {
-        //TODO
-        
+    public void set_market_account_id(int id) {
+		this.market_account_id = id;
+	}
+
+    public float get_balance() {
+        return balance;
     }
+
+    public void set_balance(float bal) {
+		this.balance = bal;
+	}
+
+
+    public String get_balance_date() {
+        return balance_date;
+    }
+
+    public void set_balance_date(String date) {
+		this.balance_date = date;
+	}
+
+    public String get_account_date() {
+        return account_date;
+    }
+
+    public void set_account_date(String date) {
+		this.account_date = date;
+	}
+
+    public String get_username() {
+        return username;
+    }
+
+    public void set_username(String name) {
+		this.username = name;
+	}
 }
--- starsrus/src/main/java/cs174/starsrus/entities/StockAccount.java
+++ starsrus/src/main/java/cs174/starsrus/entities/StockAccount.java
@@ -1,16 +1,56 @@
 package cs174.starsrus.entities;
 
 
+public class StockAccount {
 
-public class StockAccount extends Account{
-    
-    public void buy(int numberOfStocks) {
-        //TODO
-        
+    private String symbol;
+    private float balance;
+    private String original_buying_price;
+    private String account_date;
+    private String username;
+
+
+     public StockAccount(){}
+
+
+    public String get_symbol() {
+        return symbol;
     }
 
-    public void sell(int numberOfStocks) {
-        //TODO
-        
+    public void set_symbol(String sym) {
+		this.symbol = sym;
+	}
+
+    public float get_balance() {
+        return balance;
     }
+
+    public void set_balance(float bal) {
+		this.balance = bal;
+	}
+
+
+    public String get_original_buying_price() {
+        return original_buying_price;
+    }
+
+    public void set_original_buying_price(String price) {
+		this.original_buying_price = price;
+	}
+
+    public String get_account_date() {
+        return account_date;
+    }
+
+    public void set_account_date(String date) {
+		this.account_date = date;
+	}
+
+    public String get_username() {
+        return username;
+    }
+
+    public void set_username(String name) {
+		this.username = name;
+	}
 }
